f2b7daca9cab69b3c22eb9e6b398d2367a67e28d
Roy Sand <roy.sand@openpos.tech>
2021-02-16 16:41:06 +0100

    nswag creates swagger doc

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Common;
 using Application;
-using Infrastructure;
 using Infrastructure.Persistence;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 // using Microsoft.OpenApi.Models;
 
@@ -35,7 +27,7 @@ namespace API
             services.AddApplication();
             services.AddControllers();
 
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,12 +37,9 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseSwagger();
+            app.UseOpenApi();
 
-            app.UseSwaggerUI(settings =>
-            {
-                settings.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-            });
+            app.UseSwaggerUi3();
 
             app.UseCustomExceptionHandler();
                 app.UseHttpsRedirection();
